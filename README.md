8d1d4f02c2737f76230e516dc9f752d2637db80b
warmans <stefan.warman@gmail.com>
2016-06-02 19:16:55 +0200

    Added packaging include. Cleanup

--- README.md
+++ README.md
@@ -7,13 +7,12 @@ Makefile include to simplify github release automation.
 
 ### Variables
 
-You must override some variables in your makefile. The minimum required vars
-can be seem in the example makefile below. Detailed descriptions are
-available in the `github.mk` comments.
+You must override some variables in your makefile. Detailed descriptions are
+available in the makefile comments.
 
 ### Targets
 
-Targets with a `_` prefix are not indended for direct use. The only "public" target
+Targets with a `_` prefix are not intended for direct use. The only "public" target
 is `release`. For more information on these targets see the comments in the
 `github.mk` file.
 
@@ -28,5 +27,5 @@ RELEASE_VERSION = "0.0.5"
 
 RELEASE_ARTIFACT_DIR = "/path/to/some/artifacts"
 
-include github.mk
+include inc/github.mk
 ```
---
+++ inc/packaging.mk
@@ -0,0 +1,37 @@
+
+# Version identifier to use for package. This should be something like 1.0.0 (REQUIRED)
+PACKAGE_VERSION ?= $(RELEASE_VERSION)
+
+# Location to stage the package contents before creating the archive. (OPTIONAL)
+PACKAGE_CONTENT_DIR ?= .packaging
+
+# Where to put the artifact (deb or whatever) (OPTIONAL)
+PACKAGE_OUTPUT_DIR ?= .
+
+# Type of package to build. This is passed directly to fpm.
+PACKAGE_TYPE ?= deb (OPTIONAL)
+
+# The package name (REQUIRED)
+PACKAGE_NAME ?=
+
+.PHONY: package
+package: _configure_package
+
+	@if [ -z "$(shell which fpm 2>/dev/null)" ]; then \
+		echo "error:\nPackaging requires effing package manager (fpm) to run.\nsee https://github.com/jordansissel/fpm\n"; \
+		exit 1; \
+	fi
+
+	#build package
+	fpm --rpm-os linux \
+		--force \
+		-s dir \
+		-p ${PACKAGE_OUTPUT_DIR} \
+		-t ${PACKAGE_TYPE} \
+		-n ${PACKAGE_NAME} \
+		-v ${PACKAGE_VERSION} \
+		-C ${PACKAGE_CONTENT_DIR} .
+
+.PHONY: _configure_package
+_configure_package:
+	#you must override this with something that adds the relevant files to $(PACKAGE_CONTENT_DIR)
